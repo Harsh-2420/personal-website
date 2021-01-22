81e5ac42fdd4cf7fdb395d794a16d9aca968ab05
Harsh-2420 <harsh2420@gmail.com>
2021-01-21 23:22:29 -0500

    gsap and sub libs

--- website/src/App.js
+++ website/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./App.css";
 // import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -6,6 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Jumbotron } from "./Components/Jumbotron";
 import { Findr } from "./Components/Findr";
 import { Fixit } from "./Components/Fixit";
+import { TweenMax, Power3 } from "gsap";
 
 function App() {
   window.scrollTo(0, 0);
