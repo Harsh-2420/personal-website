cd16513b9fa4942a687b7d4dfd10b12e41371e4a
Harsh-2420 <harsh2420@gmail.com>
2021-01-21 19:20:37 -0500

    added new pages

---
+++ website/src/Components/Findr.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+// import { Container, Row, Col, Fade } from "react-bootstrap";
+import findrLogo from "../Images/findrLogo.svg";
+// import phone from "../Images/iphone.png";
+
+export const Findr = () => {
+  return (
+    <header className="findr">
+      <div class="container"></div>
+    </header>
+  );
+};
---
+++ website/src/Components/Fixit.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+// import { Container, Row, Col, Fade } from "react-bootstrap";
+import findrLogo from "../Images/findrLogo.svg";
+// import phone from "../Images/iphone.png";
+
+export const Fixit = () => {
+  return (
+    <header className="fixit">
+      <div class="container"></div>
+    </header>
+  );
+};
