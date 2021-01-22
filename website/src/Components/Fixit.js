06ce859ed65936be08ad1d6268669db410ac456c
Harsh-2420 <harsh2420@gmail.com>
2021-01-22 14:13:00 -0500

    scrapped scrub and changed animations

--- website/src/Components/Fixit.js
+++ website/src/Components/Fixit.js
@@ -6,39 +6,36 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 // intro = fixit & video = fixit-paper
 export const Fixit = () => {
-  const IntroVideoRef = useRef(null);
   const videoRef = useRef(null);
 
   useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-    videoRef.current.currentTime = 0;
-    ScrollTrigger.create({
-      trigger: IntroVideoRef.current,
-      scrub: true,
-      pin: IntroVideoRef.current,
-      start: "center center",
-      end: "+=2000",
-      markers: true,
-      onUpdate: function (self) {
-        if (videoRef.current) {
-          const scrollPos = self.progress;
-          const videoDuration = videoRef.current.duration;
-          const videoCurrentTime = videoDuration * scrollPos;
-
-          if (videoCurrentTime) {
-            videoRef.current.currentTime = videoCurrentTime;
-          }
-
-          // console.log(videoDuration, scrollPos, videoCurrentTime)
-        }
+    gsap.to(".fixit-paper", {
+      scale: 1.4,
+      duration: 1,
+      scrollTrigger: {
+        trigger: ".fixit-paper",
+        markers: true,
+        start: "top 600px",
+        end: "bottom 150px",
+        scrub: true,
       },
     });
-  }, [IntroVideoRef, videoRef]);
+  }, []);
 
   return (
     <header className="fixit">
-      <div ref={IntroVideoRef} class="intro">
-        <video ref={videoRef} src={paper} className="fixit-paper"></video>
+      <div class="intro">
+        <video
+          ref={videoRef}
+          src={paper}
+          className="fixit-paper"
+          autostart
+          autoplay="autoplay"
+          muted="muted"
+          playsInline
+          disablePictureInPicture
+          type="video/mp4"
+        />
       </div>
     </header>
   );
