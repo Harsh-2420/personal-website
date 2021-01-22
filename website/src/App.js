2b08353fab3b4b4d88d048f535b7c8dc886ad924
Harsh-2420 <harsh2420@gmail.com>
2021-01-22 00:41:28 -0500

    scrollTrigger set up

--- website/src/App.js
+++ website/src/App.js
@@ -1,15 +1,19 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import "./App.css";
 // import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import { gsap } from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import { TweenMax, Power3 } from "gsap";
+
 import { Jumbotron } from "./Components/Jumbotron";
 import { Findr } from "./Components/Findr";
 import { Fixit } from "./Components/Fixit";
-import { TweenMax, Power3 } from "gsap";
 
 function App() {
   window.scrollTo(0, 0);
+
   return (
     <div className="App">
       <Jumbotron />
--- website/src/Components/Findr.js
+++ website/src/Components/Findr.js
@@ -1,13 +1,30 @@
-import React, { useState } from "react";
+import React, { useRef, useEffect, useState } from "react";
 // import { Container, Row, Col, Fade } from "react-bootstrap";
-import findrLogo from "../Images/findrLogo.svg";
-// import phone from "../Images/iphone.png";
+import findrLogo from "../Images/uoft.png";
+
+import { gsap } from "gsap";
 
 export const Findr = () => {
+  const logoItem = useRef(null);
+
+  useEffect(() => {
+    gsap.to(".findrLogo", {
+      x: 100,
+      duration: 5,
+      scrollTrigger: {
+        trigger: ".findrLogo",
+        markers: true,
+        start: "top center",
+        end: "bottom 80px",
+        scrub: true,
+      },
+    });
+  }, []);
+
   return (
     <header className="findr">
       <div class="container">
-        <img src={findrLogo} className="findrLogo" />
+        <img ref={logoItem} src={findrLogo} className="findrLogo" />
       </div>
     </header>
   );
