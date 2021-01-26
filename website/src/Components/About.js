4534b094ca016d3219266ed08318655bd0dc733f
Harsh-2420 <harsh2420@gmail.com>
2021-01-26 13:49:54 -0500

    About content done

--- website/src/Components/About.js
+++ website/src/Components/About.js
@@ -27,19 +27,29 @@ export const About = () => {
   return (
     <header className="about">
       <div class="container">
-        <Col md={4}>
+        <Col md={3}>
           {/* <div class="side-head">ABOUT</div>
         <div class="side-head-2">ABOUT</div> */}
         </Col>
-        <Col md={4}>
+        <Col md={6}>
           <div class="container-inner">
             <div class="about-title">Who am I</div>
             <div class="about-desc">
               I'm an entrepreneur, a tech head and an aspiring Data Scientist
+              <br />- <br />I love having control - I mean who doesn't - but
+              what I mean is having control over the process. This website for
+              example - I control the design and the architecture. For my
+              projects, I try to streamline the process to make the different
+              teams work in tandem.
+              <br />
+              This is what draws me towards data. Working with big data provides
+              the kind of control I like. The feeling of being overwhelmed by
+              millions of datapoints and making sense of it. Manipulating it and
+              visualising it to make algorithms - It makes me feel alive!
             </div>
           </div>
         </Col>
-        <Col md={4}></Col>
+        <Col md={3}></Col>
       </div>
     </header>
   );
